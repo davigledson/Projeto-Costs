864bf06344c13d76ee5752e91a2412b6818d25b8
davigledson <davigledson@outlook.com>
2023-01-08 04:09:01 -0300

    atualizando a home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,16 @@
+
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
 function Home(){
-    return <h1>Home</h1>
+    return (
+        <section className={styles.home_container}>
+            <h1>Bem-Vindo ao <span>Costs</span></h1>
+            <p>Comece a gerenciar seus projetos agora mesmo!</p>
+            <LinkButton to='/newproject' text='Criar Projeto'/>
+            <img src={savings} alt='Costs'/>
+        </section>
+    
+    )
 }
 export default Home
