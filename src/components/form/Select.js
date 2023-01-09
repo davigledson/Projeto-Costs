7cb99933ddb4b1a6dbfa380ea370d8036c18d58c
davigledson <davigledson@outlook.com>
2023-01-09 00:10:01 -0300

    Criando a API do projeto

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,10 +1,18 @@
 import styles from './Select.module.css'
-function Select({ text, name,options, handleOnChange, value}){
+function Select({ text, name, options, handleOnChange, value}){
     return(
         <div className={styles.form_control}>
+
             <label htmlFor={name}>{text}:</label>
+
             <select name={name}  id={name} >
                 <option>Selecione uma opção</option>
+                {options.map((option)=>(
+                    <option value={option.id} key={option.id}>
+                              {option.name}
+                    </option>
+                ))}
+
              </select>
         </div>
     )
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -2,7 +2,27 @@ import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
+
+import { useEffect, useState } from 'react'
+
 function ProjectForm({btnText}){
+    const [categories, setCategories] =useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories",{
+        method:'GET',
+        headers:{
+            'Content-type':'application.json'
+        }
+        
+    })
+    .then((resp)=> resp.json())
+    .then((data)=>{
+        setCategories(data)
+    })
+    .catch((err)=> console.log(err))
+    }, [])
+
     return  (
     
     <form className={styles.form}>
@@ -23,7 +43,7 @@ function ProjectForm({btnText}){
       
         
         
-       <Select name='category_id' text='Selecione a categoria'/>
+       <Select name='category_id' text='Selecione a categoria' options={categories}/>
         <SubmitButton text={btnText}/>
     </form>
      )
